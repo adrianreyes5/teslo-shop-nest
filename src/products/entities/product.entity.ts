7decd9c0c298643d7ff0f12cb255609f10e61bcb
Adrian reyes <adriandmc05@gmail.com>
2022-11-08 12:02:25 -0400

    add relationship

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -3,8 +3,10 @@ import {
   BeforeUpdate,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { ProductImage } from './index';
 
 @Entity()
 export class Product {
@@ -49,6 +51,13 @@ export class Product {
     default: [],
   })
   tags: string[];
+
+  // images
+  @OneToMany(() => ProductImage, (productImage) => productImage.product, {
+    cascade: true,
+    eager: true,
+  })
+  images?: ProductImage[];
 
   @BeforeInsert()
   checkSlugInsert() {
